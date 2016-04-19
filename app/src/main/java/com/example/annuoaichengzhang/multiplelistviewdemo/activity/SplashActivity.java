9948b88bcf07e8a7b1369ffdf0e17c40371acf12
安诺爱成长 <1399487511@qq.com>
2016-04-19 19:54:00 +0800

    为底部变换留出入口

---
+++ app/src/main/java/com/example/annuoaichengzhang/multiplelistviewdemo/activity/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.example.annuoaichengzhang.multiplelistviewdemo.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.annuoaichengzhang.multiplelistviewdemo.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        findViewById(R.id.mt_lv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        
+    }
+
+}
